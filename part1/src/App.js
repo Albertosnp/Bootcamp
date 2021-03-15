8fb0500ceb3159406f9cbc4ef6235077a9564e74
Alberto Snp <albertosnp91@gmail.com>
2021-03-15 18:44:28 +0100

    Commit part1-react

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Mensaje from './Mensaje.js'
+
+const Description = () => {
+  return <p>
+    Esta es la app del curso de fullstack bootcamp
+  </p>
+};
 
 function App() {
   return (
     <div className="App">
-      
+      <Mensaje color='red' message='Estamos trabajando'/>
+      <Mensaje color='blue' message='En un curso'/> 
+      <Mensaje color='green' message='de React'/>      
+      <Description /> 
     </div>
   );
 }
